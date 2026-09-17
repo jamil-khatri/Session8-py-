c13ddf0a8a8e01927d27b6fe6317d79c04673987
jamil-khatri <jkhatri37capp@gmail.com>
2026-09-17 17:15:07 +0530

    Add product description and word analysis

---
+++ Product_description.py
@@ -0,0 +1,7 @@
+description = "Wireless headphones with noise cancellation and long battery life"
+
+words = description.split()
+
+print("First word:", words[0])
+print("Last word:", words[-1])
+print("Total number of words:", len(words))
